330a500f76c16db12075b6c8000103bb46af3355
Howard M. Harte <hharte@magicandroidapps.com>
2023-04-01 14:13:53 -0700

    AltairZ80: Z180: SD Memory card support.
    Add support for an SD memory card connected to the Z180's CSIO
    interface.  Compatible with RomWBW.

--- AltairZ80/altairz80_sys.c
+++ AltairZ80/altairz80_sys.c
@@ -107,6 +107,7 @@ extern DEVICE djhdc_dev;
 
 extern DEVICE z180io_dev;
 extern DEVICE z180asci_dev;
+extern DEVICE rc_sdcard_dev;
 
 extern long disasm (unsigned char *data, char *output, int segsize, long offset);
 extern t_stat parse_sym_m68k(char* c, t_addr a, UNIT* u, t_value* val, int32 sw);
@@ -200,6 +201,7 @@ DEVICE      *sim_devices[]  = {
     /* Zilog Z180 */
     &z180io_dev,
     &z180asci_dev,
+    &rc_sdcard_dev,
     NULL
 };
 
